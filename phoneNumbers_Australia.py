8876850fc74ad9482f060d53ba09e764baab06fe
Brendon Pinniger <brendon.pinniger@zoom.us>
2023-02-21 11:32:21 +1100

    update

--- phoneNumbers_Australia.py
+++ phoneNumbers_Australia.py
@@ -4,29 +4,24 @@ import logging
 
 logging.basicConfig(filename="logfile.log", encoding='utf-8', level=logging.DEBUG)
 
+#get site details - we will need the site Id to update the phone numbers
 sites = Zoom.listPhoneSites()
 Brisbane = next(site for site in sites if site["name"] == "Brisbane")
-print (Brisbane["id"])
 main = next(site for site in sites if site["name"] == "Main Site")
-print (main["id"])
 
+#get unassigned phone numbers
 numbers = Zoom.getPhoneNumbers()
-
-
 brisbaneNumbers = []
 mainSiteNumbers = []
 
 for number in numbers:
     phoneNumber = number["number"]
-    print (phoneNumber[0:4])
-    print (number["site"]["name"])
     if phoneNumber[0:4] == "+617":
         brisbaneNumbers.append(number["id"])
     else:
         mainSiteNumbers.append(number["id"])
 
-
-pp.pprint(brisbaneNumbers)
+#update phone numbers for each site Id
 if len(brisbaneNumbers) > 0:
     Zoom.updatePhoneNumbers(Brisbane["id"], brisbaneNumbers)
 
